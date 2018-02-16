1efdc7a03505eca805b0507a345a5eb9599adc54
TurkojanOwnz <david9108@hotmail.com>
2018-02-16 12:54:38 -0200

    Update Caneta.java

--- Aula02/src/aula02/Caneta.java
+++ Aula02/src/aula02/Caneta.java
@@ -3,33 +3,84 @@ package aula02;
 
 
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
+    private string modelo;
+    private string cor;
+    private float ponta;
+    private int carga;
+    private boolean tampada;
     
-    void rabiscar(){
+    public void SetModelo(string modelo)
+    {
+        //PODEM SER FEITAS AS VALIDAÇÔES NO METÓDO SETTER
+        this.modelo = modelo;
+    }
+    
+    public string GetModelo()
+    {
+        return(this.modelo);
+    }
+    
+    public void SetCor(string cor)
+    {
+        this.cor = cor;
+    }
+    
+    public string GetCor()
+    {
+        return(this.cor);
+    }
+    
+    public void SetPonta(float ponta)
+    {
+        this.ponta = ponta;
+    }
+    
+    public float GetPonta()
+    {
+        return(this.ponta);
+    }
+    
+    public void SetCarga(int carga)
+    {
+        this.carga = carga;
+    }
+    
+    public int GetCarga()
+    {
+        return(this.carga);
+    }
+    
+    public void SetEstadoTampa(boolean Estado)
+    {
+        this.tampada = Estado;
+    }
+    
+    public boolean GetEstadoTampa()
+    {
+        return(this.tampada);   
+    }
+    
+    void Rabiscar(){
         if (this.tampada == true) {
-            System.out.println("ERRO não posso rabiscar!!!");
+            System.out.println("Erro ao tentar rabiscar! Caneta tampada.");
         } else {
             System.out.println("Estou rabiscando.");
         }
     }
     
-    void tampar(){
-        this.tampada = true;
+    void Tampar(){
+        SetEstadoTampa(true);
     }
     
-    void destampar(){
-        this.tampada = false;
+    void Destampar(){
+        SetEstadoTampa(false);
     }
     
-    void status(){
-        System.out.println("Modelo: " + this.modelo );
-        System.out.println("Uma caneta: " + this.cor);
-        System.out.println("Ponta: " + this.ponta);
-        System.out.println("Carga: " + this.carga);
-        System.out.println("Esta tampada? " + this.tampada);
+    void Status(){
+        System.out.println("Modelo: " + GetModelo());
+        System.out.println("Uma caneta: " + GetCor());
+        System.out.println("Ponta: " + Float.toString(GetPonta()));
+        System.out.println("Carga: " + Integer.toString(GetCarga()));
+        System.out.println("Esta tampada? " + Boolean.toString(GetEstadoTampa()));
     }
 }
